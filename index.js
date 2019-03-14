347d483dd6dca8f34c70a101d4598dd0478459b0
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-14 03:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ keepGoing(array, changeValue, skipValue) which iterates through array and change
 findBy(array, findFn) which looks for a value in array based on the return value of findFn. Return null if the value isn't found. Hint: Check the test file to know the signature of the findFn and the type of its return value
 */
 function breakOut(array, changeValue, stopValue) {
-  for( let i = 0; i < array.length; i++) {
+  for( let i = 0; i < array.length; i += 1) {
     if( c +== stopValue) {
       break;
     }
     array[i] = changeValue;
   }
   return array;
+}
+function keepGoing(array, changeValue, skipValue) {
+  for(let i = 0; i < array.length; i += 1) {
+    if(array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+
 }
